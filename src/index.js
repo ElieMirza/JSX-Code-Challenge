35e12928deb434c5531ed92ebebada24f8854469
Elie Mirza <83608815+ElieMirza@users.noreply.github.com>
2022-04-01 21:37:31 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+//Create a react app from scratch.
+import React from "react";
+import ReactDOM from "react-dom";
+//It should display a h1 heading.
+ReactDOM.render(
+  //It should display an unordered list (bullet points).
+  <div>
+    <h1>Elie Mirza</h1>
+    <ul>
+      <li>list 1</li>
+      <li>list 2</li>
+      <li>list 3</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
+
+//It should contain 3 list elements.
